1fd0531f4894ca1fbebd5e2b1b62a5b218c9b166
Jason <jasonjasyno50@gmail.com>
2020-12-02 13:37:38 +0100

    adding final content and styles for frogger page

--- src/pages/frogger/frogger.page.jsx
+++ src/pages/frogger/frogger.page.jsx
@@ -72,6 +72,86 @@ const FroggerPage = () => {
                     </div>
                 </div>
             </section>
+
+            <main className="project-description">
+                <img
+                    id="frogger-img"
+                    className="project-homepage-img"
+                    src="https://res.cloudinary.com/dynamicjson/image/upload/v1606904947/frogger-mobile_tnswqc.png"
+                    alt="Sceenshot of the frogger game about to the begin."
+                />
+
+                <section
+                    className="project-region why-region"
+                    aria-labelledby="frogger-stack-why-heading"
+                    aria-describedby="frogger-stack-why-description"
+                >
+                    <h3 id="frogger-stack-why-heading">Why I chose the stack I used</h3>
+
+                    <p id="frogger-stack-why-description">
+                        Well, mostly, I had to. The project was designed to test how much I'd learned
+                        about OOP (Object-Oriented Programming) concepts and not cooking up spaghetti code.
+                        It was a lovely challenge at the time and I enjoyed taking it on.
+                    </p>
+                </section>
+
+                <section
+                    className="project-region problems-region"
+                    aria-labelledby="frogger-problems-heading"
+                    aria-describedby="frogger-problems-description"
+                >
+                    <h3 id="frogger-problems-heading">Problems I ran into.</h3>
+
+                    <p id="frogger-problems-description">
+                        My biggest challenge working on this project, which was more engaging than it was
+                        frustrating thankfully, was figuring out how to listen for swipe movements
+                        on touch screens. I found an article that helped with this and then I figured the rest from there.<br />
+                        <br />
+                        I had to spend time setting up speed levels for the bugs and sprite, setting up logic for
+                        when felt natural to make the sprite collide with the bugs. I tried out different values and
+                        eventually settled on what I felt was good enough.
+                    </p>
+                </section>
+
+                <div className="project-region project-screenshots">
+                    <div id="frogger-img-group" className="img-group">
+                        <img
+                            src="https://res.cloudinary.com/dynamicjson/image/upload/v1606907235/frogger-pick-character_n9wmdr.png"
+                            alt="Screenshot of the patients request page for doctors."
+                        />
+
+                        <img
+                            src="https://res.cloudinary.com/dynamicjson/image/upload/v1606908378/frogger-timed-mode_hrhklk.png"
+                            alt="Full-page screenshot of the homepage of the frogger site."
+                        />
+
+                        <img
+                            src="https://res.cloudinary.com/dynamicjson/image/upload/v1606908085/frogger-timed-mode_bktlzt.png"
+                            alt="Screenshot of the patient request page."
+                        />
+                    </div>
+                </div>
+
+                <section
+                    className="project-region learned-lessons"
+                    aria-labelledby="frogger-lessons-heading"
+                    aria-describedby="frogger-lessons-description"
+                >
+                    <h3 id="frogger-lessons-heading">What I learned and added to the project</h3>
+
+                    <p id="frogger-lessons-description">
+                        I learned how to listen for touch events. I had fun practising my ternary operators.
+                        I also improved my research skills.
+                        <br /><br />
+                        I felt swiping was a more intuitive action while playing the game on mobile touch screens so
+                        I felt I'd add that instead of putting buttons to work similarly to the keyboard listeners.
+                        I also added a timed mode where you had two minutes to score as many points as you can. I also
+                        tried to style the project in a unique way and ended up achieving that.
+                    </p>
+                </section>
+            </main>
+
+            <ProjectsFooter />
         </div>
     )
 }
