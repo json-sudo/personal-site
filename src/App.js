4e7186085171cffb03a7f5c68e0a9dca90b8a6ac
Jason <jasonjasyno50@gmail.com>
2020-12-07 13:19:23 +0100

    splitting page routes with react suspense and lazy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { useRef } from 'react'
+import { useRef, Suspense, lazy } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Header from './components/header/header.component'
-
-import CrownClothingPage from './pages/crown-clothing/crown-clothing.page'
-import ContactPage from './pages/contact/contact.page'
-import DrupexPage from './pages/drupex/drupex.page'
-import FroggerPage from './pages/frogger/frogger.page'
-import HomePage from './pages/home/home.page'
-import ResumePage from './pages/resume/resume.page'
-import ProjectsPage from './pages/projects/projects.page'
-import SpodPage from './pages/spod/spod.page'
+import LoaderComponent from './components/loader/loader.component'
 
 import './App.scss'
+
+const ContactPage = lazy(() => import('./pages/contact/contact.page'))
+const CrownClothingPage = lazy(() => import('./pages/crown-clothing/crown-clothing.page'))
+const DrupexPage = lazy(() => import('./pages/drupex/drupex.page'))
+const HomePage = lazy(() => import('./pages/home/home.page'))
+const ProjectsPage = lazy(() => import('./pages/projects/projects.page'))
+const FroggerPage = lazy(() => import('./pages/frogger/frogger.page'))
+const ResumePage = lazy(() => import('./pages/resume/resume.page'))
+const SpodPage = lazy(() => import('./pages/spod/spod.page'))
 
 function App() {
 	const appRef = useRef(null)
@@ -20,21 +21,20 @@ function App() {
 		<div ref={appRef} className="App">
 			<Header appDomElementRef={appRef} />
 
-			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route path="/contact-me" component={ContactPage} />
-				<Route path="/resume" component={ResumePage} />
-				<Route exact path="/projects" component={ProjectsPage} />
-				<Route path="/projects/crown-clothing" component={CrownClothingPage} />
-				<Route path="/projects/drupex" component={DrupexPage} />
-				<Route path="/projects/spod" component={SpodPage} />
-				<Route path="/projects/frogger" component={FroggerPage} />
-			</Switch>
+			<Suspense fallback={<LoaderComponent loading={true} />}>
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/contact-me" component={ContactPage} />
+					<Route path="/resume" component={ResumePage} />
+					<Route exact path="/projects" component={ProjectsPage} />
+					<Route path="/projects/crown-clothing" component={CrownClothingPage} />
+					<Route path="/projects/drupex" component={DrupexPage} />
+					<Route path="/projects/spod" component={SpodPage} />
+					<Route path="/projects/frogger" component={FroggerPage} />
+				</Switch>
+			</Suspense>
 		</div>
 	);
 }
 
 export default App;
-
-// TODO
-// Code split page routes and figure out how to split css bundles as well
