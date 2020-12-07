a3e47fbbc53b93ce155f34c20aba3e24905f79ac
Jason <jasonjasyno50@gmail.com>
2020-12-07 18:04:14 +0100

    fixing styles of a project pages messing up because of suspense? Adding grayscale behaviour for images and svg in dark mode

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import { Switch, Route } from 'react-router-dom'
 import Header from './components/header/header.component'
 import LoaderComponent from './components/loader/loader.component'
 
+import CrownClothingPage from './pages/crown-clothing/crown-clothing.page'
+import DrupexPage from './pages/drupex/drupex.page'
+import FroggerPage from './pages/frogger/frogger.page'
+import ProjectsPage from './pages/projects/projects.page'
+import SpodPage from './pages/spod/spod.page'
+
 import './App.scss'
 
 const ContactPage = lazy(() => import('./pages/contact/contact.page'))
-const CrownClothingPage = lazy(() => import('./pages/crown-clothing/crown-clothing.page'))
-const DrupexPage = lazy(() => import('./pages/drupex/drupex.page'))
 const HomePage = lazy(() => import('./pages/home/home.page'))
-const ProjectsPage = lazy(() => import('./pages/projects/projects.page'))
-const FroggerPage = lazy(() => import('./pages/frogger/frogger.page'))
 const ResumePage = lazy(() => import('./pages/resume/resume.page'))
-const SpodPage = lazy(() => import('./pages/spod/spod.page'))
 
 function App() {
 	const appRef = useRef(null)
@@ -21,18 +22,21 @@ function App() {
 		<div ref={appRef} className="App">
 			<Header appDomElementRef={appRef} />
 
-			<Suspense fallback={<LoaderComponent loading={true} />}>
+				<Suspense fallback={<LoaderComponent loading={true} />}>
+					<Switch>
+						<Route exact path="/" component={HomePage} />
+						<Route path="/contact-me" component={ContactPage} />
+						<Route path="/resume" component={ResumePage} />
+					</Switch>
+				</Suspense>
+
 				<Switch>
-					<Route exact path="/" component={HomePage} />
-					<Route path="/contact-me" component={ContactPage} />
-					<Route path="/resume" component={ResumePage} />
 					<Route exact path="/projects" component={ProjectsPage} />
 					<Route path="/projects/crown-clothing" component={CrownClothingPage} />
 					<Route path="/projects/drupex" component={DrupexPage} />
 					<Route path="/projects/spod" component={SpodPage} />
 					<Route path="/projects/frogger" component={FroggerPage} />
 				</Switch>
-			</Suspense>
 		</div>
 	);
 }
