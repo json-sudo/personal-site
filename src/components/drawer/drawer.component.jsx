1110ae2338c0a5f93cb07fb047453420d911334e
Jason <jasonjasyno50@gmail.com>
2020-12-07 14:26:35 +0100

    adding transition to drawer component for mobile without messing up tab order

--- src/components/drawer/drawer.component.jsx
+++ src/components/drawer/drawer.component.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { Transition } from 'react-transition-group'
 
 import Backdrop from '../backdrop/backdrop.component'
 
@@ -21,26 +22,32 @@ const DrawerNav = () => {
         <>
             {backdrop}
 
-            <div className={`drawer-container ${drawerIsOpen ? 'open' : ''}`}>
-                <header className="drawer-header">
-                    <button
-                        className="close-btn"
-                        aria-label="Close navigation drawer"
-                        onClick={() => closeDrawer()}
-                    >
-                        &times;
-                    </button>
-                </header>
+            <Transition in={drawerIsOpen} timeout={300}>
+                {
+                    state => (
+                        <div className={`drawer-container drawer-container-${state}`}>
+                            <header className="drawer-header">
+                                <button
+                                    className="close-btn"
+                                    aria-label="Close navigation drawer"
+                                    onClick={() => closeDrawer()}
+                                >
+                                    &times;
+                                </button>
+                            </header>
 
-                <nav>
-                    <ul className="drawer-nav">
-                        <li onClick={() => closeDrawer()}><Link to="/">About Me</Link></li>
-                        <li onClick={() => closeDrawer()}><Link to="/resume">CV/Resume</Link></li>
-                        <li onClick={() => closeDrawer()}><Link to="/projects">Projects</Link></li>
-                        <li onClick={() => closeDrawer()}><Link to="/contact-me">Leave a Message</Link></li>
-                    </ul>
-                </nav>
-            </div>
+                            <nav>
+                                <ul className="drawer-nav">
+                                    <li onClick={() => closeDrawer()}><Link to="/">About Me</Link></li>
+                                    <li onClick={() => closeDrawer()}><Link to="/resume">CV/Resume</Link></li>
+                                    <li onClick={() => closeDrawer()}><Link to="/projects">Projects</Link></li>
+                                    <li onClick={() => closeDrawer()}><Link to="/contact-me">Leave a Message</Link></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    )
+                }
+            </Transition>
         </>
     )
 }
