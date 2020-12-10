fe532595878253d2c830902b2ef99bc60c762add
Jason <jasonjasyno50@gmail.com>
2020-12-10 18:45:49 +0100

    reverting routes to work without suspense

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import { useRef, useState, Suspense, lazy } from 'react'
+import { useRef, useState } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Header from './components/header/header.component'
-import LoaderComponent from './components/loader/loader.component'
 
 import BlogPage from './pages/blog/blog.page'
+import ContactPage from './pages/contact/contact.page'
+import CreditsPage from './pages/credits/credits.page'
 import CrownClothingPage from './pages/crown-clothing/crown-clothing.page'
 import DrupexPage from './pages/drupex/drupex.page'
+import Error404Page from './pages/page-not-found/error-404.page'
 import FroggerPage from './pages/frogger/frogger.page'
+import HomePage from './pages/home/home.page'
 import ProjectsPage from './pages/projects/projects.page'
+import ResumePage from './pages/resume/resume.page'
 import SpodPage from './pages/spod/spod.page'
 import ToptalBlogpostPage from './pages/toptal-blogpost/toptal-blogpost.page'
 
 import animationsAreOnContext from './contexts/animations.context'
 
 import './App.scss'
-
-const ContactPage = lazy(() => import('./pages/contact/contact.page'))
-const CreditsPage = lazy(() => import('./pages/credits/credits.page'))
-const Error404Page = lazy(() => import('./pages/page-not-found/error-404.page'))
-const HomePage = lazy(() => import('./pages/home/home.page'))
-const ResumePage = lazy(() => import('./pages/resume/resume.page'))
 
 function App() {
 	const appRef = useRef(null)
@@ -36,29 +34,24 @@ function App() {
 		<div ref={appRef} className="App">
 			<Header appDomElementRef={appRef} />
 
-				<Suspense fallback={<LoaderComponent loading={true} />}>
-					<Switch>
-						<Route exact path="/">
-							<animationsAreOnContext.Provider value={animationsAreEnabledState}>
-								<HomePage />
-							</animationsAreOnContext.Provider>
-						</Route>
-						<Route path="/contact-me" component={ContactPage} />
-						<Route path="/resume" component={ResumePage} />
-						<Route path="/credits" component={CreditsPage} />
-					</Switch>
-				</Suspense>
-
-				<Switch>
-					<Route exact path="/blog" component={BlogPage} />
-					<Route exact path="/projects" component={ProjectsPage} />
-					<Route path="/projects/crown-clothing" component={CrownClothingPage} />
-					<Route path="/projects/drupex" component={DrupexPage} />
-					<Route path="/projects/spod" component={SpodPage} />
-					<Route path="/projects/frogger" component={FroggerPage} />
-					<Route path="/blog/toptal-application" component={ToptalBlogpostPage} />
-					<Route component={Error404Page} />
-				</Switch>
+			<Switch>
+				<Route exact path="/">
+					<animationsAreOnContext.Provider value={animationsAreEnabledState}>
+						<HomePage />
+					</animationsAreOnContext.Provider>
+				</Route>
+				<Route path="/contact-me" component={ContactPage} />
+				<Route path="/resume" component={ResumePage} />
+				<Route path="/credits" component={CreditsPage} />
+				<Route exact path="/blog" component={BlogPage} />
+				<Route exact path="/projects" component={ProjectsPage} />
+				<Route path="/projects/crown-clothing" component={CrownClothingPage} />
+				<Route path="/projects/drupex" component={DrupexPage} />
+				<Route path="/projects/spod" component={SpodPage} />
+				<Route path="/projects/frogger" component={FroggerPage} />
+				<Route path="/blog/toptal-application" component={ToptalBlogpostPage} />
+				<Route component={Error404Page} />
+			</Switch>
 		</div>
 	);
 }
