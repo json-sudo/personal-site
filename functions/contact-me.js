99df81fcfc4da6e7150068a4550b9c4ad2aa4f89
Jason <jasonjasyno50@gmail.com>
2020-12-27 19:18:25 +0100

    The Fixing: 12434535343292903rd time's a charm

--- functions/contact-me.js
+++ functions/contact-me.js
@@ -5,39 +5,72 @@ const OAuth2 = google.auth.OAuth2;
 require('dotenv').config()
 const { clientId, clientSecret, refreshToken  } = process.env
 
-// fixing acesstoken expiring
-// from https://medium.com/@nickroach_50526/sending-emails-with-node-js-using-smtp-gmail-and-oauth2-316fe9c790a1
-const oauth2Client = new OAuth2(
-    clientId,
-    clientSecret,
-    "https://developers.google.com/oauthplayground"
-);
-
-oauth2Client.setCredentials({
-    refresh_token: refreshToken
-});
-const accessToken = oauth2Client.getAccessToken()
-
 exports.handler = function(event, context, callback) {
 
     let data = JSON.parse(event.body)
 
-    let transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            type: "OAuth2",
-            user: "jason.omemu@gmail.com",
+    // fixing acesstoken expiring
+    // from https://dev.to/chandrapantachhetri/sending-emails-securely-using-node-js-nodemailer-smtp-gmail-and-oauth2-g3a
+    const createTransporter = async () => {
+        const oauth2Client = new OAuth2(
             clientId,
             clientSecret,
-            refreshToken,
-            accessToken: accessToken
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
+            "https://developers.google.com/oauthplayground"
+        );
 
-    transporter.sendMail({
+        oauth2Client.setCredentials({
+            refresh_token: refreshToken
+        });
+
+        // wrapping in a promise as getAccessToken requires a callback and doesn't
+        // support async/await
+        const accessToken = await new Promise((resolve, reject) => {
+            oauth2Client.getAccessToken((err, token) => {
+                if (err) {
+                reject("Failed to create access token");
+                }
+                resolve(token);
+            });
+        });
+
+        let transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                type: "OAuth2",
+                user: "jason.omemu@gmail.com",
+                clientId,
+                clientSecret,
+                refreshToken,
+                accessToken
+            },
+        });
+
+        return transporter
+    }
+
+    const sendMail = async emailData => {
+        let emailTransporter = await createTransporter();
+
+        await emailTransporter.sendMail(emailData, function(error, info) {
+            if (error) {
+                callback(error, {
+                    body: JSON.stringify({
+                        'status': false,
+                    })
+                });
+            } else {
+                callback(null, {
+                statusCode: 200,
+                body: JSON.stringify({
+                    'status': true,
+                    'message': 'Your message was sent successfully. Thanks for reaching out, I\'d get back to you as soon as possible 😊.'
+                })
+            });
+            }
+        });
+    }
+
+    sendMail({
         from: data.email,
         to: "jason.omemu@gmail.com",
         subject: data.subject,
@@ -45,21 +78,5 @@ exports.handler = function(event, context, callback) {
             <h3>Email from ${data.name} ${data.email}<h3>
             <p>${data.message}<p>
         `
-    }, function(error, info) {
-        if (error) {
-            callback(error, {
-                body: JSON.stringify({
-                    'status': false,
-                })
-            });
-        } else {
-            callback(null, {
-            statusCode: 200,
-            body: JSON.stringify({
-                'status': true,
-                'message': 'Your message was sent successfully. Thanks for reaching out, I\'d get back to you as soon as possible 😊.'
-            })
-        });
-        }
-    });
+    })
 }
